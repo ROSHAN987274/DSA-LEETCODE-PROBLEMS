561834ec5b7e8f8a2f7ae2c9c0cadfcbe7fc16a8
Roshan Chaurasiya <chaurasiyaroshan801@gmail.com>
2026-07-14 23:41:23 +0530

    Create Reverse_a_single_Linkedlist.c

---
+++ Linkedlist/Reverse_a_single_Linkedlist.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+     struct ListNode *temp;
+
+    while(curr != NULL){
+       temp = curr->next;
+        curr ->next = prev;
+        prev = curr;
+        curr = temp;
+        
+    }
+    return prev;
+
+ 
+}
+
+
